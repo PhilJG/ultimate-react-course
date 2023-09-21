63e3b59174a0e9d3101e819f6fd6ad5e7387e81f
Phil J Gray <phl.j.gray@gmail.com>
2023-09-21 16:33:21 -0700

    Completed section 6

--- 04-steps/starter/counter/src/App.js
+++ 04-steps/starter/counter/src/App.js
@@ -1,63 +1,52 @@
 import { useState } from "react";
+import "./styles.css";
 
-//Date Counter
-//step # = how high the count goes up on each press
-// count number of +/- days to the current date
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
 
-// export default function App() {
-//   return (
-//     // const [count, setCount] = useState(0);
-//     <div className="App">
-//       <Counter name="Step" />
-//       {/* <Counter name="Count" /> */}
-//     </div>
-//   );
-// }
-
-export default function App({ name }) {
-  const [step, setStep] = useState(0);
+function Counter() {
+  const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
   // const [date, setDate] = useState(new Date());
 
   let date = new Date();
   date.setDate(date.getDate() + count);
-  // let options = {
-  //   weekday: "short",
-  //   year: "numeric",
-  //   month: "short",
-  //   day: "numeric",
-  // };
-  // let formatDate = date.toLocaleDateString("en-US", options);
-  // let currentDate = new Intl.DateTimeFormat("en-US", {
-  //   dateStyle: "full",
-  // }).format(date);
 
-  // alert(
-  //   new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(currentDate)
-  // );
-
-  // function addDate(date) {
-  //   date.setDate(date.getDate() + step);
-
-  //   return date;
-  // }
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
 
   return (
     <>
-      {/* In your current setup, you're immediately invoking the setStep function when the component renders, which changes the state, causing a re-render, which again invokes setStep, and so on. This creates an infinite loop of re-renders, hence the error. */}
-      {/* <button onClick={setStep((s) => s - 1)}>-</button>
-      {name}: {step}
-    <button onClick={setStep((s) => s + 1)}>+</button> */}
-      {/* Step */}
       <div>
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(+e.target.value)}
+        />
         <span>Step: {step}</span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
       <div>
         {/* Count */}
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
+        <span>
+          <input
+            type="number"
+            value={count}
+            onChange={(e) => {
+              e.preventDefault();
+              setCount(+e.target.value);
+            }}
+          />
+        </span>
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <p>
@@ -69,6 +58,11 @@ export default function App({ name }) {
           : `${count} days ago was `}
         <span>{date.toDateString()}</span>
       </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </>
   );
 }
