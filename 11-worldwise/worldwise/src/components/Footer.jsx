e2b453635f05002aa36b1d9b38c9e1810522debe
Phil <phl.j.gray@gmail.com>
2023-12-31 03:05:41 -0800

    Complete 212: Building App layout

---
+++ 11-worldwise/worldwise/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./SideBar.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.copyright}>&copy; Copyright 2023 WorldWise Inc.</p>
+    </footer>
+  );
+}
+
+export default Footer;
