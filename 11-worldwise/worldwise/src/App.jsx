136d456d789504643bbc08ec6fe477395228d5b4
Phil <phl.j.gray@gmail.com>
2023-12-31 01:47:08 -0800

    Completed 209

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -20,6 +21,10 @@ function App() {
         <Route
           path="pricing"
           element={<Pricing />}
+        />
+        <Route
+          path="app"
+          element={<AppLayout />}
         />
         <Route
           path="*"
--- 11-worldwise/worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/worldwise/src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function HomePage() {
   return (
     <div>
       <PageNav />
-      <h1>HomePage</h1>
-      <Link to="/pricing">Pricing</Link>
+      <AppNav />
+      <h1 className="test">HomePage</h1>
+      <Link to="/app">Got to the App</Link>
     </div>
   );
 }
