2cdfa0a58881e0e214f1c9c9b2f0a8beb3bf3384
Phil <phl.j.gray@gmail.com>
2023-12-31 02:14:34 -0800

    Complete 210: Building the Pages of the SPA

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -5,6 +5,7 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -21,6 +22,10 @@ function App() {
         <Route
           path="pricing"
           element={<Pricing />}
+        />
+        <Route
+          path="login"
+          element={<Login />}
         />
         <Route
           path="app"
