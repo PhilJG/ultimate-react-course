13aa2b09791e682311a8d97328c751054e8eacc6
Phil <phl.j.gray@gmail.com>
2023-12-31 18:00:09 -0800

    Complete 213: Implementing the Cities List

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/Homepage";
@@ -6,13 +8,35 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+
+const BASE_URL = "http://localhost:9000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route
-          path="/"
+          index
           element={<HomePage />}
         />
         <Route
@@ -30,7 +54,34 @@ function App() {
         <Route
           path="app"
           element={<AppLayout />}
-        />
+        >
+          <Route
+            index
+            path="cities"
+            element={
+              <CityList
+                cities={cities}
+                isLoading={isLoading}
+              />
+            }
+          />
+          <Route
+            element={
+              <CityList
+                cities={cities}
+                isLoading={isLoading}
+              />
+            }
+          />
+          <Route
+            path="countries"
+            element={<p>Countries</p>}
+          />
+          <Route
+            path="form"
+            element={<p>Form</p>}
+          />
+        </Route>
         <Route
           path="*"
           element={<PageNotFound />}
